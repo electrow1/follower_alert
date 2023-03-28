647851a3e14453322af2b674cf85104ab50a4d5e
electrow1 <81616373+electrow1@users.noreply.github.com>
2023-03-28 20:53:57 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-no_minutes=2
+no_minutes=5
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=get_all_updates, trigger="interval", seconds=60*no_minutes)
 scheduler.start()
