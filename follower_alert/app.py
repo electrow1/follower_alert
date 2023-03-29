0ede041efd69176e82213582a18be16e88751736
electrow1 <81616373+electrow1@users.noreply.github.com>
2023-03-29 05:32:29 +0100

    Update app.py

--- follower_alert/app.py
+++ follower_alert/app.py
@@ -1,1 +1,15 @@
 
+from apscheduler.schedulers.background import BackgroundScheduler
+from helpers import get_all_updates
+from flask import Flask
+
+
+app = Flask(__name__)
+
+no_minutes=2
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=get_all_updates, trigger="interval", seconds=60*no_minutes)
+scheduler.start()
+
+if __name__ == "__main__":
+    app.run(debug=False)
